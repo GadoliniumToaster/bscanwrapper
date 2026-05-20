56c7cd0f4625a97f327854f214e10acd98c63635
plathyst <icerbasi@pm.me>
2026-05-20 18:21:35 -0400

    Basic Functionality

--- scanner.py
+++ scanner.py
@@ -1,2 +1,31 @@
-import sane #Using python-sane-2.9.2
-print('Test')
+import subprocess
+
+# ID for my Brother DS 720D
+SANE_SCAN_ID = 'dsseries:usb:0x04F9:0x60E0'
+
+# Can also do PDF, probably the only two options that will ever be used.
+FORMAT = 'png'
+
+#Linear | Color | Gray
+MODE = 'Gray'
+
+#What's DPI again?
+DPI = '300'
+
+# US Letter
+SCAN_COORDS = ('215.9', '279.4')
+
+def scan():
+    subprocess.run([
+        'scanimage',
+        f'--device-name={SANE_SCAN_ID}',
+        f'--format={FORMAT}', f'--mode={MODE}',
+        f'--resolution={DPI}',
+        '-x', SCAN_COORDS[0],
+        '-y', SCAN_COORDS[1],
+        f'--output-file=test_scan.png'
+        ]
+        )
+
+if __name__ == "__main__":
+    scan()
